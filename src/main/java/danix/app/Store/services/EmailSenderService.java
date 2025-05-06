365bf778d2ae003b02762687904f980cf19eb1fd
Daniil Cheban <danikcheban2007@gmail.com>
2025-05-06 20:51:12 +0300

    Added docker

--- src/main/java/danix/app/Store/services/EmailSenderService.java
+++ src/main/java/danix/app/Store/services/EmailSenderService.java
@@ -2,6 +2,7 @@ package danix.app.Store.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
@@ -10,10 +11,12 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class EmailSenderService {
     private final JavaMailSender mailSender;
+    @Value("${spring.mail.username}")
+    private String email;
 
     public void sendMessage(String to, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(""); //enter your email
+        message.setFrom(email);
         message.setTo(to);
         message.setText(text);
         message.setSubject("Spring-store-application");
